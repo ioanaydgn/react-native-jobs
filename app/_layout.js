72da188fe404ddddc88a24cdcecb140561c7b50e
Ioan Mert Aydogan <mertaydogn0@gmail.com>
2023-04-19 00:39:50 +0300

    Home page 50%

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,22 @@
 import { Stack } from 'expo-router';
+import { useCallback } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-    return <Stack />;
-}
+  const [fontsLoaded] = useFonts({
+    DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+    DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+    DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) await SplashScreen.hideAsync();
+  }, [fontsLoaded]);
+
+  return fontsLoaded && <Stack onLayoutRootView={onLayoutRootView} />;
+};
 
 export default Layout;
